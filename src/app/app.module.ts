b852436f3a8284ab3b536717fdb2ddea8e565d4d
Alex <alex.gordon@optum.com>
2020-08-25 22:18:28 -0400

    added bootstrap and added customers table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { NgbNav, NgbNavItem, NgbNavLink, NgbNavContent } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavComponent } from './shared/nav/nav.component';
 import { CustomerComponent } from './views/customer/customer.component';
 import { HomeComponent } from './views/home/home.component';
@@ -29,12 +28,8 @@ import { AdminComponent } from './views/admin/admin.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
-    // NgbModule,
-    // NgbNav,
-    // NgbNavItem,
-    // NgbNavLink,
-    // NgbNavContent
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
